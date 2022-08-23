40776c3c5ba420660eb195a64128383ed2aa0caf
bobo mallick <mallickbobo@gmail.com>
2022-08-23 10:44:07 +0530

    Greater Two

---
+++ video_20/GreaterTwo.java
@@ -0,0 +1,16 @@
+// WAP to find the larger of two number.
+
+class GreaterTwo
+{
+   public static void main(String[] args)
+   {
+    int a =10, b = 20;
+    if(a>b)
+    {
+        System.out.println("a is Greater");
+    }else
+    {
+        System.out.println("b is Greater");
+    }
+   } 
+} 
