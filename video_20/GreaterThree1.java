1763785211b5c9fca6fc2be41731bcfc345ad29b
bobo mallick <mallickbobo@gmail.com>
2022-08-23 11:22:35 +0530

    Greater Three1

---
+++ video_20/GreaterThree1.java
@@ -0,0 +1,17 @@
+class GreaterThree1
+{
+    public static void main(String[] args)
+    {
+        int a=10, b=20, c=30;
+        if(a>b && a>c)
+        {
+            System.out.println("a is Greater");
+        }else if(b>a && b>c)
+        {
+            System.out.println("b is Greater");
+        }else
+        {
+            System.out.println("c is Greater");
+        }
+    }
+}
