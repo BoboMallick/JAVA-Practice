73105fff7781a62995c537206bdc6406ee2c1ce4
bobo mallick <mallickbobo@gmail.com>
2022-08-24 09:45:12 +0530

    Even Odd number

---
+++ EvenOdd.java
@@ -0,0 +1,14 @@
+class EvenOdd
+{
+    public static void main(String[] args)
+    {
+        int no = 31;
+        if(no%2==0)
+        {
+            System.out.println("Even number");
+        }else
+        {
+            System.out.println("Odd number");
+        }
+    }
+}
