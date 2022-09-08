c0f3ac58414ecb759448cef8a36db54758f15b24
bobo mallick <mallickbobo@gmail.com>
2022-09-08 22:24:55 +0530

    Reverse a String with for loop statement

---
+++ 30_reverse string/ReverseAString.java
@@ -0,0 +1,14 @@
+class ReverseAString
+{
+    public static void main(String[] args)
+    {
+        String name = "Keya";
+        String rev = "";
+        int len = name.length();
+        for(int i=len-1; i>=0; i--)
+        {
+            rev=rev+name.charAt(i);
+        }
+        System.out.println("Reverse of "+name+" is "+ rev);
+    }
+}
